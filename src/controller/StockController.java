c3243c9fde514dc6072da0c02f6f386ca0b1e55d
Carlos Muñoz <munozrengifocarlos@gmail.com>
2023-04-29 18:14:50 -0500

    refactor: Code styling for StockController

--- src/controller/StockController.java
+++ src/controller/StockController.java
@@ -2,8 +2,6 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import model.Product;
-import model.ProductRepository;
 import model.StockRepository;
 import view.StockWindow;
 
